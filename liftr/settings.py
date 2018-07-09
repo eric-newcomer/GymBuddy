a6b8ef100e50374a6ab5099802a1fc4424833364
Eric Newcomer <enewcomer23@gmail.com>
2018-07-08 20:39:44 -0700

    Got the CSS connected!! (trust me, this is hard with django)

--- liftr/settings.py
+++ liftr/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    '/liftr/liftr/main/static/'
+]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-   return HttpResponse("TEMPORARY")
+   return render(request, 'main/home.html')
