06124e2074e2ab84af86d8888926c114096bbcd7
Eric Newcomer <ericnewcomer@microvu.local>
2018-07-09 17:01:49 -0700

    Add about template and links

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
    return render(request, 'main/home.html')
+
+def about(request):
+   return render(request, 'main/about.html')
