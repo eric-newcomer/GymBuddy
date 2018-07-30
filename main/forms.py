5dba88df542b2dd0ef51e563cb667b138da832f5
Eric Newcomer <ericnewcomer@microvu.com>
2018-07-30 11:17:56 -0700

    Adding corrected form

--- main/forms.py
+++ main/forms.py
@@ -10,4 +10,4 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
