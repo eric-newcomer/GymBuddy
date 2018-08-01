5835eb75cbfd4ebce9b7d60ee4755937b6baca51
Eric Newcomer <ericnewcomer@microvu.com>
2018-08-01 10:12:30 -0700

    Fix admin access

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Gym
+from .models import Post, Gym, Profile
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Gym)
+admin.site.register(Profile)
