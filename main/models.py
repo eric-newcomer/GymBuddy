d3bf30ed0072fbebd31845d7ffc569705750777f
Eric Newcomer <ericnewcomer@microvu.com>
2018-08-03 17:40:59 -0700

    Add image field to profile model

--- main/models.py
+++ main/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     activity1 = models.CharField(max_length=50, blank=True)
     activity2 = models.CharField(max_length=50, blank=True)
     website = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
         return self.user.username
